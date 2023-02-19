58f974e80e52433a70e219305f68bfc35bba582a
Crowdbotics <team@crowdbotics.com>
2023-02-19 17:14:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "epay-39016",
+  url: "https://epay-39016.botics.co",
+  api: "https://epay-39016.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
